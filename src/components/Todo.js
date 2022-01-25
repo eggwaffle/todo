79caeb1df5918f52d1b82839558a826b708c76a2
eggwaffle <29174802+eggwaffle@users.noreply.github.com>
2022-01-26 01:36:11 +1030

    Add delete button

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,20 +3,37 @@ import { useState } from "react";
 function Todo(props) {
   const [checked, setChecked] = useState(props.completed);
   let completed = ``;
-  props.completed ? completed = `completed` : completed = ``;
+  let hidden = ``;
+  if (props.completed) {
+    completed = `completed`
+    hidden = ``
+  } else {
+    completed = ``;
+    hidden = `hidden`;
+  }
 
   return (
-    <div className="todo">
-      <input
-        id={props.id}
-        className="todo-checkbox"
-        type="checkbox"
-        checked={checked}
-        onChange={() =>setChecked(props.onChange(props.id, checked))}
-      />
-      <p className={`todo-text ${completed}`}>
-        {props.content}
-      </p>
+    <div className="todo-container">
+      <div className="todo">
+        <input
+          id={props.id}
+          className="todo-checkbox"
+          type="checkbox"
+          checked={checked}
+          onChange={() =>setChecked(props.onChange(props.id, checked))}
+        />
+        <p className={`todo-text ${completed}`}>
+          {props.content}
+        </p>
+      </div>
+      <button
+        className={`delete-todo ${hidden}`}
+        onClick={() => console.log("delete")}
+      >
+        <span className="material-icons delete">
+          delete_outline
+        </span>
+      </button>
     </div>
   );
 }
