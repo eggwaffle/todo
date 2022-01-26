f638b15c662e6955be1a52a43d5d9d5d25e4cc2e
eggwaffle <29174802+eggwaffle@users.noreply.github.com>
2022-01-26 14:33:12 +1030

    Add localStorage setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,29 @@
+import { useState, useEffect} from "react";
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+
+  useEffect(() => {
+    if (localStorage.getItem('todos') === null) {
+      localStorage.setItem('todos', JSON.stringify(todoList))
+      console.log("new todo list")
+    } else {
+      let data = JSON.parse(localStorage.getItem('todos'));
+      console.log(data)
+      setTodoList(data);
+    }
+  },[]);
+
   return (
     <div className="App">
       <Header />
-      <Main />
+      <Main
+        todoList={todoList}
+        setTodoList={setTodoList}
+      />
       <Footer />
     </div>
   );
